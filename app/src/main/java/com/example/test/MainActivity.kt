662336813450979ff1dfc8222501d462051da460
FaLo <loerch.f@googlemail.com>
2021-05-28 12:37:01 +0200

    Played around with spinners onSelected method.

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        
+        spinner.onItemSelectedListener = this
     }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        val item = parent.getItemAtPosition(position)
+        val text: TextView = findViewById(R.id.textView)
+
+        text.text = item.toString()
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+        val text: TextView = findViewById(R.id.textView)
+
+        text.text = "Nothing selected"
+    }
+
+
 }
